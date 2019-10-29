a7b1cb767ab8844e941ca2d5f3a94e5bfd471570
Student <example@example.com>
2019-10-29 15:06:11 -0700

    add functionality which replaces vowels in user input with a dash and display word puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
-  $("button#submit").click(function(ev) {
+
+  $("button#submit").click(function(ev){
+
     ev.preventDefault();
     // console.log("hi");
 
-    var userInput = $("#userInput").val();
+    var userInput = $("#userInput").val().split("");
     console.log(userInput);
 
+
     $("#puzzleResults").text(userInput)
-      // $("p#puzzleResults").text(userInput);
-    });
-  });
+
+    var vowels = ["a","e","i","o","u"]
+
+    for(index = 0; index < userInput.length; index++ ) {
+      // if (vowels.includes(userInput[index]){
+        //
+        // }
+
+        var doesItInclude = vowels.includes(userInput[index]);
+        var swissSentence = []
+
+        if (doesItInclude == true) {
+          console.log(doesItInclude);
+          userInput[index] = "-";
+          console.log(userInput);
+        }
+
+      };
+
+      $("#puzzleResults").text(userInput.join(""));
+
+      // if user input at index =vowel replace with -
+
+      // if $("#puzzleResults").has(vowels)) {
+        //   userInput.replace(vowels,"-");
+        // } else {
+          //   alert("OOPS!");
+          // }
+        });
+      });
